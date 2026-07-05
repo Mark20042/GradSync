a095cb7da720d7d2f521a801cb16135b8bfb2091
Mark20042 <mark.potot2004@gmail.com>
2026-07-05 10:49:04 +0800

    fix when opening a assessment it will automatically goes down

--- client/src/pages/Assessment/components/PreAssessmentAgreement.jsx
+++ client/src/pages/Assessment/components/PreAssessmentAgreement.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Shield,
   AlertTriangle,
@@ -10,6 +10,10 @@ import {
 
 const PreAssessmentAgreement = ({ assessment, onAgree, onCancel }) => {
   const [agreed, setAgreed] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
 
   const rules = [
     {
