51d7b230292ee71999128825d01d2b208a5ffcad
Mark20042 <mark.potot2004@gmail.com>
2026-07-09 23:34:33 +0800

    remove the color of the button back to home

--- client/src/pages/Documentation/Documentation.jsx
+++ client/src/pages/Documentation/Documentation.jsx
@@ -46,7 +46,7 @@ const Documentation = () => {
       {/* Hero Section */}
       <div className="bg-white relative">
         <div className="absolute top-4 left-4 sm:top-8 sm:left-8">
-          <Link to="/" className="inline-flex items-center text-sm font-medium text-gray-600 hover:text-gray-900 transition-colors bg-gray-100 hover:bg-gray-200 px-4 py-2 rounded-xl">
+          <Link to="/" className="inline-flex items-center text-sm font-medium text-gray-600 hover:text-gray-900 transition-colors  px-4 py-2">
             <svg className="w-4 h-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
             </svg>
